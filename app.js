046ae4ca0500f081b752a847c26b8cd3ab093777
mandrej <milan.andrejevic@gmail.com>
2025-11-10 18:50:20 +0100

    .

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
-  // res.send('Hello World! This is my small Express site.');
 });
 
 app.get('/data', (req, res) => {
@@ -25,21 +24,6 @@ app.get('/data', (req, res) => {
         console.error('Error reading CSV:', error);
         res.status(500).send('Error processing CSV file.');
     });
-});
-
-app.get('/data/data.csv', (req, res) => {
-  // Fetch data from database or other source
-  // const data = [
-  //   { date: '10/26/2025', time: '11:42', value: 8.5 },
-  //   { date: '10/26/2025', time: '11:52', value: 7.5 },
-  //   { date: '10/26/2025', time: '12:05', value: 6.5 },
-  //   { date: '10/27/2025', time: '11:42', value: 8.2 },
-  //   { date: '10/27/2025', time: '11:52', value: 7.2 },
-  //   { date: '10/27/2025', time: '12:10', value: 5.8 },
-   
-  // ];
-  res.download(path.join(__dirname, 'data', 'data.csv'));
-  // res.json(data);
 });
 
 app.listen(PORT, () => {
